0c2123921123e3997d5734ddbecdc1728c1abe93
Zach Thornton <thorntonz@wit.edu>
2014-07-19 15:46:52 -0400

    Made turtles smaller

--- src/Level.ts
+++ src/Level.ts
@@ -2,12 +2,18 @@
     export class Level extends Phaser.State {
         turtles: Phaser.Group;
         turtle: Phaser.Sprite;
+
+        txt: Phaser.Group;
+        timeText:Phaser.Text;
+        shellsCollected: Phaser.Text;
+        initialTime: number;
       
 
         create() {
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
             this.turtles = this.game.add.group();
+            this.txt = this.game.add.group();
 
             for (var i = 0; i < 30; i++) {
                 this.turtle = this.turtles.create(this.game.rnd.integerInRange(100, 700), this.game.rnd.integerInRange(100, 400), 'turtle');
@@ -21,9 +27,17 @@
             this.turtles.setAll('body.bounce.y', 1);
             this.turtles.setAll('body.minBounceVelocity', 0);
 
+            
+            this.txt.fixedToCamera = true;
+            this.initialTime = this.game.time.now;
+            this.timeText = this.game.add.text(this.game.world.x,0,'Time: '+this.initialTime,
+                        {fontSize: '32px', fill: 'white', stroke: 'black',strokeThickness: 5}, this.txt);
+            this.txt.bringToTop(true);
+
         }
 
         update() {
+            this.timeText.setText(Math.floor(this.game.time.elapsedSince(this.initialTime)/1000).toString());
             this.game.physics.arcade.collide(this.turtles);
         }
 
--- src/game.js
+++ src/game.js
@@ -60,6 +60,7 @@ var Wallaby;
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
             this.turtles = this.game.add.group();
+            this.txt = this.game.add.group();
 
             for (var i = 0; i < 30; i++) {
                 this.turtle = this.turtles.create(this.game.rnd.integerInRange(100, 700), this.game.rnd.integerInRange(100, 400), 'turtle');
@@ -72,9 +73,15 @@ var Wallaby;
             this.turtles.setAll('body.bounce.x', 1);
             this.turtles.setAll('body.bounce.y', 1);
             this.turtles.setAll('body.minBounceVelocity', 0);
+
+            this.txt.fixedToCamera = true;
+            this.initialTime = this.game.time.now;
+            this.timeText = this.game.add.text(this.game.world.x, 0, 'Time: ' + this.initialTime, { fontSize: '32px', fill: 'white', stroke: 'black', strokeThickness: 5 }, this.txt);
+            this.txt.bringToTop(true);
         };
 
         Level.prototype.update = function () {
+            this.timeText.setText(Math.floor(this.game.time.elapsedSince(this.initialTime) / 1000).toString());
             this.game.physics.arcade.collide(this.turtles);
         };
         return Level;
